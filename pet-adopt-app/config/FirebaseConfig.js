7f9f78c1c894b322356783ccbfa50698e3c2e302
zsakhi3 <zsakhi3@gatech.edu>
2025-01-22 20:36:18 -0500

    backend is acting stupid

---
+++ pet-adopt-app/config/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "pet-adopt-app-cc961.firebaseapp.com",
+  projectId: "pet-adopt-app-cc961",
+  storageBucket: "pet-adopt-app-cc961.firebasestorage.app",
+  messagingSenderId: "404211160067",
+  appId: "1:404211160067:web:55b475e913c4581dcfaa36",
+  measurementId: "G-Z0HKMBWK4Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
