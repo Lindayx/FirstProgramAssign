4f08d1a58fae710e917afbb21a03ab467867b779
zsakhi3 <zsakhi3@gatech.edu>
2025-01-22 21:33:00 -0500

    finished slider and backend section - next is DISPLAY CATEGORY

--- pet-adopt-app/app/(tabs)/home.jsx
+++ pet-adopt-app/app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
       {/* Header */}
         <Header />
       {/* Slider */}
-
+        <Slider />
       {/* Cateory */}
 
       {/* List Of Pets */}
---
+++ pet-adopt-app/components/Home/Slider.jsx
@@ -0,0 +1,51 @@
+import { getDocs, collection } from 'firebase/firestore';
+import React from 'react';
+import { Dimensions, StyleSheet, View, FlatList, Image } from 'react-native';
+import { db } from '../../config/FirebaseConfig';
+
+export default function Slider() {
+    
+    const [sliderList, setSliderList] = React.useState([]);
+    React.useEffect(()=>{
+        GetSliders();
+    },[]);
+
+    const GetSliders=async()=>{
+        setSliderList([]);
+        const snapshot=await getDocs(collection(db, 'Sliders'));
+        snapshot.forEach((doc)=>{
+            console.log(doc.data());
+            setSliderList(sliderList=>[...sliderList, doc.data()])
+        })
+    }
+
+  return (
+    <View style={{
+        marginTop:15
+    }}
+    
+    >
+      <FlatList 
+        data={sliderList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item, index})=>(
+            <View>
+                <Image source={{uri:item?.imageUrl}} 
+                    style={styles?.sliderImage}
+                />
+            </View>
+         )}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    sliderImage:{
+        width:Dimensions.get('screen').width*0.9,
+        height:170,
+        borderRadius:15,
+        marginRight:15
+    }
+})
