5c0d64db63882cd087c48ed72f49822207737811
Linda Xiao <yinfengx@umich.edu>
2025-01-24 17:07:45 -0500

    code ready for category list, need firebase access

--- pet-adopt-app/app/(tabs)/home.jsx
+++ pet-adopt-app/app/(tabs)/home.jsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
+import PetListByCategory from '../../components/Home/PetListByCategory'
 
 const Home = () => {
   return (
@@ -13,8 +14,8 @@ const Home = () => {
         <Header />
       {/* Slider */}
         <Slider />
-      {/* Cateory */}
-
+      {/* Petlist + Cateory */}
+        {/* <PetListByCategory /> */}
       {/* List Of Pets */}
 
       {/* Add New Pet Option */}
