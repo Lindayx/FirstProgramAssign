cc2e0928700e4f455b961967ed4b052cb79c2c1c
Linda Xiao <yinfengx@umich.edu>
2025-01-25 13:11:49 -0500

    button - add new pet

--- pet-adopt-app/app/(tabs)/home.jsx
+++ pet-adopt-app/app/(tabs)/home.jsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
 import PetListByCategory from '../../components/Home/PetListByCategory'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Colors from '../../constants/Colors'
 
-const Home = () => {
+export default function Home(){
   return (
     <View style={{
       padding:20, marginTop:20
@@ -19,8 +21,31 @@ const Home = () => {
       {/* List Of Pets */}
 
       {/* Add New Pet Option */}
+      <TouchableOpacity style={styles.addNewPetContainer}>
+        <MaterialIcons name="pets" size={24} color={Colors.PRIMARY} />
+        <Text style={{
+          fontFamily:'outfir-medium',
+          color:Colors.PRIMARY,
+          fontSize:18
+        }}>Add New Pet</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-export default Home
+const styles = StyleSheet.create({
+  addNewPetContainer:{
+    display:'flex',
+    flexDirection:'row',
+    gap:10,
+    alignItems:'center',
+    padding:20,
+    marginTop:20,
+    backgroundColor:Colors.LIGHT_PRIMARY,
+    borderWidth:1,
+    borderColor:Colors.PRIMARY,
+    borderRadius:15,
+    borderStyle:'dashed',
+    justifyContent:'center'
+  }
+})
