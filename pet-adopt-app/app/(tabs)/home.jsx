8325213120825cc35782d47ce0ba9b1df7cc214d
Linda Xiao <yinfengx@umich.edu>
2025-02-14 03:45:23 -0500

    add pet done with dummy image url

--- pet-adopt-app/app/(tabs)/home.jsx
+++ pet-adopt-app/app/(tabs)/home.jsx
@@ -5,6 +5,7 @@ import Slider from '../../components/Home/Slider'
 import PetListByCategory from '../../components/Home/PetListByCategory'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Colors from '../../constants/Colors'
+import  {Link} from "expo-router";
 
 export default function Home(){
   return (
@@ -21,14 +22,15 @@ export default function Home(){
       {/* List Of Pets */}
 
       {/* Add New Pet Option */}
-      <TouchableOpacity style={styles.addNewPetContainer}>
+      <Link href = {'/add-new-pet'}
+      style={styles.addNewPetContainer}>
         <MaterialIcons name="pets" size={24} color={Colors.PRIMARY} />
         <Text style={{
           fontFamily:'outfir-medium',
           color:Colors.PRIMARY,
           fontSize:18
         }}>Add New Pet</Text>
-      </TouchableOpacity>
+      </Link>
     </View>
   )
 }
@@ -41,6 +43,7 @@ const styles = StyleSheet.create({
     alignItems:'center',
     padding:20,
     marginTop:20,
+    textAlign:'center',
     backgroundColor:Colors.LIGHT_PRIMARY,
     borderWidth:1,
     borderColor:Colors.PRIMARY,
