bcc13d9f1aafc0e5b6cf3fe7d3d27e03a5618c09
PramaYudhistira <pyudhistira02@gmail.com>
2025-03-25 18:58:24 -0400

    setting up backend server, re-implemented logout

--- pet-adopt-app/app/(tabs)/profile.jsx
+++ pet-adopt-app/app/(tabs)/profile.jsx
@@ -1,10 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
+import { useNavigation  } from 'expo-router'
 
 export default function Profile() {
+  const { signOut } = useAuth()
+
+  const navigation = useNavigation()
+
+  const handleSignOut = async () => {
+    await signOut()
+    navigation.navigate('/login')
+  }
   return (
     <View>
       <Text>Profile</Text>
+      <button onClick={handleSignOut}>Sign Out</button>
     </View>
   )
 }
